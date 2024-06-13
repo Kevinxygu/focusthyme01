0652157e64bceb87b55ae134f2cbd22b26939dbf
KevinGooby <87846274+KevinGooby@users.noreply.github.com>
2024-06-12 22:22:03 -0700

    started styling everything

--- src/components/Main/Main.styles.ts
+++ src/components/Main/Main.styles.ts
@@ -1,5 +1,52 @@
 import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+* {
+margin: 0;
+padding: 0;
+font-family: "Roboto", sans-serif;
+}
+`
 
 export const Container = styled.div`
-    color: red;
+    width: 276px;
+    height: 333px;
+    text-align: center;
 `;
+
+export const menuContainer = styled.div`
+    padding: 10px;
+    cursor: pointer;
+    transition: 0.3s ease;
+    top: 20px;
+    left: 20px;
+    position: absolute;
+
+
+    &:hover {
+    opacity: 0.7;
+    }
+`;
+
+export const menuIcon = styled.img`
+    max-width: 30px;
+`;
+
+export const smallHeader = styled.h2`
+color: grey;
+font-weight: 200;
+margin-top: 50px;
+`
+
+export const bigHeader = styled.h1`
+color: blue;
+font-weight: 400;
+`
+
+export const bigButton = styled.div`
+width: 10px;
+height: 10px;
+background-color: red;
+`
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,14 +3,29 @@ import * as Styled from './Main.styles';
 
 const Main = () => {
     const STORAGE_KEY: string = "list"
-    const [isActive, setIsActive] = useState<boolean>(true);
+    const [isActive, setIsActive] = useState<boolean>(false);
     const [websiteList, setWebsiteList] = useState<string[]>([]);
 
 
     return (
+        <>
+        <Styled.GlobalStyle />
         <Styled.Container>
-            <h1>Hello World!</h1>
+            <Styled.menuContainer>
+                <Styled.menuIcon src='images/menu.png'/>
+            </Styled.menuContainer>
+
+            <Styled.smallHeader>
+                Focus mode is
+            </Styled.smallHeader>
+            <Styled.bigHeader>
+                {isActive ? "ON" : "OFF"}
+            </Styled.bigHeader>
+            <Styled.bigButton>
+
+            </Styled.bigButton>
         </Styled.Container>
+        </>
     )
 }
 
