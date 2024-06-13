e2d98d0db7438280814fd97c2c6d18bd578f0cd3
KevinGooby <87846274+KevinGooby@users.noreply.github.com>
2024-06-12 21:54:06 -0700

    started building out main react component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as Styled from './Main.styles';
 
 const Main = () => {
+    const STORAGE_KEY: string = "list"
+    const [isActive, setIsActive] = useState<boolean>(true);
+    const [websiteList, setWebsiteList] = useState<string[]>([]);
+
+
     return (
         <Styled.Container>
             <h1>Hello World!</h1>
