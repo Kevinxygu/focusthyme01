80e452829d839d460b979995644a72ade54f7c56
KevinGooby <87846274+KevinGooby@users.noreply.github.com>
2024-06-29 17:54:51 -0700

    removed testing code and added comments for readability

--- scripts/content.js
+++ scripts/content.js
@@ -1,21 +1,23 @@
-console.log("This is the content!");
-
 // content.js is what alters the HTML CSS to block the website
 const STORAGE_KEY_LIST = "list";
 const STORAGE_KEY_ACTIVE = "active";
 
 // const toBlock = ["www.reddit.com", "www.youtube.com", "www.taobao.com"];
+
+// retrieves current domain (eg. youtube.com, reddit.com, etc.)
 const currentDomain = window.location.hostname;
 
+// load in active from chrome storage API
 chrome.storage.sync.get([STORAGE_KEY_ACTIVE], (resultActive) => {
+
+    // get the active value
     const active = resultActive[STORAGE_KEY_ACTIVE];
 
+    // load in list of websites from chrome storage API
     chrome.storage.sync.get([STORAGE_KEY_LIST], (resultList) => {
         const list = resultList[STORAGE_KEY_LIST] || [];
-        
-        // alert(JSON.stringify(list));
-        // alert(JSON.stringify(active));
 
+        // check: if running and the current domain is part of the banned list, go and block the page by loading image
         if (list.some(site => currentDomain.includes(site)) && !active) {
             document.body.innerHTML = '<p> pp </p> <div id="image"></div>';
 
@@ -29,6 +31,7 @@ chrome.storage.sync.get([STORAGE_KEY_ACTIVE], (resultActive) => {
             img.style.zIndex = '10000';
             img.style.pointerEvents = 'none'; // Prevent interaction with the overlay
 
+            // appends the image
             document.getElementById("image").appendChild(img);
         }
     });
